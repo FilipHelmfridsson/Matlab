c6183a0ee61dc4d86bc7ee93fd949d8667140a54
FilipHelmfridsson <filip@helmfridsson.se>
2025-11-12 21:07:38 +0100

    Uppg 6LU

--- labb1-LinjarAlgebra-fackverk/uppgift6LU.m
+++ labb1-LinjarAlgebra-fackverk/uppgift6LU.m
@@ -4,20 +4,23 @@ clear;      % Tar bort alla gamla variabler
 clc;        % Rensar kommandofönstret
 close all   % Stänger alla figurer
 
-n =100; % antal iterationer;
+n =10; % antal iterationer;
 
 % Kran 1
 load('kran1.mat')
 %whos % => A Size 250x250 då 127 noder -2 som sitter i väggen * 2 frihetsgrader x och y led
-A = sparse(A);  % Glesar ut matrisen
+A = sparse(A);
 %Skapa en b vektor med rätt storlek
 %b = zeros(250,1); %250  A's size är 250x250
 b = rand(250,1); %250  A's size är 250x250
-size = [250];
+size = 250;
 time=0;
+%LU -faktorisering
+[L,U, P] = lu(A);
 for i= 1:n
     timeStart = cputime;
-    z=A\b;
+    y = L\(P*b);
+    z = U\y;
     time = time + (cputime - timeStart);
 end
 tider = [time/n];
@@ -32,9 +35,12 @@ A = sparse(A);
 b = rand(696,1);
 size = [size, 696];
 time=0;
+%LU -faktorisering
+[L,U, P] = lu(A);
 for i= 1:n
     timeStart = cputime;
-    z=A\b;
+    y = L\(P*b);
+    z = U\y;
     time = time + (cputime - timeStart);
 end
 tider = [tider, time/n];
@@ -49,9 +55,12 @@ A = sparse(A);
 b = rand(1502,1);
 size = [size, 1502];
 time=0;
+%LU -faktorisering
+[L,U, P] = lu(A);
 for i= 1:n
     timeStart = cputime;
-    z=A\b;
+    y = L\(P*b);
+    z = U\y;
     time = time + (cputime - timeStart);
 end
 tider = [tider, time/n];
@@ -60,15 +69,18 @@ fprintf('Medeltid kran 3 = %.4f s\n', time/n);
 % Kran 4
 load('kran4.mat')
 %whos % => A Size 2856x2856 då 1430 noder -2 som sitter i väggen * 2 frihetsgrader x och y led
-A = sparse(A);
+A = sparse(A);  % Glesar ut matrisen
 %Skapa en b vektor med rätt storlek
 %b = zeros(2856,1); 
 b = rand(2856,1);
 size = [size, 2856];
 time=0; 
+%LU -faktorisering
+[L,U, P] = lu(A);
 for i= 1:n
     timeStart = cputime;
-    z=A\b;
+    y = L\(P*b);
+    z = U\y;
     time = time + (cputime - timeStart);
 end
 tider = [tider, time/n];
@@ -78,27 +90,17 @@ figure;
 loglog(size, tider, 'o-', 'LineWidth', 1.5);
 xlabel('Antal obekanta');
 ylabel('Tidsåtgång [s]');
+title("CPU-tid för Gausselimination på fackverk av olika storlek Uppgift 6 LU");
 
-grid on;
 
-size
-tider
-% Från uppgiften:
-%Om tidsåtgången lyder potenssambandet T ≈ CN^α, där T är tidsåtgången och N är antalet obekanta 
-% och C, α konstanter, så kan plottning i loglog-skala ge exponenten α som lutningen
-%av resulterande linjen. Detta följer av att ta logaritmen på båda sidor av potenssambandet, vilket ger
-%log T ≈log C+ αlog N.
+% Med LU faktoriseringen
+%Medeltid kran 1 = 0.0000 s
+%Medeltid kran 2 = 0.0000 s
+%Medeltid kran 3 = 0.0010 s
+%Medeltid kran 4 = 0.0030 s
 
-% Linjen blir ganska rak vilket tyder på att sambandet håller hyfsat bra.
-% Lutningen kan uppskattas med hjälp av polyfit:
-p = polyfit(log(size), log(tider), 1) % 1 betyder linjär passning
-lutning = p(1); % Lutningen är första koefficienten
-fprintf('Uppskattad lutning α = %.2f\n', lutning);   
-title("CPU-tid för Gausselimination på fackverk av olika storlek Uppgift 6, lutning "+lutning);
 
-% Uppskattad lutnong α = 1.12
-
-% Med Sparse
+% Utan LU faktorisering
 %Medeltid kran 1 = 0.0003 s
 %Medeltid kran 2 = 0.0005 s
 %Medeltid kran 3 = 0.0015 s
