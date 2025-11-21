7846d9cbfe08defb1827332c759501a7efbb153f
abb22axerox <axelbsr2006@icloud.com>
2025-11-21 14:53:42 +0100

    update

--- Axel/Labb 2/uppg_2.m
+++ Axel/Labb 2/uppg_2.m
@@ -1,2 +1,71 @@
-y = @(x, B) (exp(-B*x))*(1 + (x/9)^3);
+B = 0.1268794;   % 0.05 ≤ B ≤ 0.4
+
+y = @(x) (exp(-B.*x) + 0.6) .* (1 + (x/9).^3);
+
+% Integral 
+% pi * från 0 till 26 integral(y^2)dx
+
+% Upphöjt i två för rotationsvolym
+f = @(x) (y(x)).^2;
+
+a = 0;
+b = 26;
+
+n = [2,4,8,16,32,64,128,256,512,1024,2048];
+
+volymer = zeros(size(n));
+
+for i = 1:length(n)
+    h = (b-a)/n(i);
+    x = a:h:b;
+
+    trapetsregeln = h * ( 0.5*f(x(1)) + sum(f(x(2:end-1))) + 0.5*f(x(end)) );
+
+    volymer(i) = pi * trapetsregeln;
+end
+
+disp(volymer)
+
+%% Felskattning
+delta_trunk = abs(diff(volymer(1:length(n))));
+
+kvoter = delta_trunk(1:end-1) ./ delta_trunk(2:end);
+
+disp('Abs skillnader:')
+disp(delta_trunk)
+disp('Kvoter:')
+disp(kvoter)
+
+% Vid 16 steg blir kvoten cirka 4, alltså har man ett 
+% rätt säkert svar där, ungefär en decimal.
+
+T8 = volymer(3); % T(h/8)
+T16 = volymer(4); % T(h/16)
+T32 = volymer(5); % T(h/32)
+
+delta1 = T16 - T8;
+delta2 = T32 - T16;
+
+kvot_check = delta1/delta2; % ger typ 4
+
+delta1_ny = delta1 / 3;
+delta2_ny = delta2 / 3;
+
+T16_hatt = delta1_ny + T16;
+T32_hatt = delta2_ny + T32;
+
+delta_rich = abs(T16_hatt - T32_hatt);
+
+fprintf('Första värdet: %.6f ± %.6f\n', T32, abs(delta2));
+fprintf('Bättre rich värde: %.6f ± %.6f\n', T32_hatt, delta_rich);
+
+kvot_felgranser = delta_rich / delta2;
+
+fprintf('Andel i skillnad av felgränserna: %.6f', abs(kvot_felgranser)); % Minskar med 99,4 procentenheter
+
+
+
+
+
+
 
---
+++ Axel/Labb 2/uppg_2c.m
@@ -0,0 +1,62 @@
+clear all
+
+
+%% Uppgift 2 c) Hitta B värde så volym hos vattentorner blir 3500
+
+% volymer - 3500 = 0 sekantmetoden
+B0 = 0.05;
+B1 = 0.1;
+riktigvolym = 3500;
+
+a = 0;
+b = 26;
+n = [2,4,8,16,32,64,128,256,512,1024,2048];
+
+y = @(x, B) (exp(-B.*x) + 0.6) .* (1 + (x/9).^3);
+
+f = @(x, B) (y(x, B)).^2; % Rotationsvolym
+
+
+olika_B = zeros(size(n));
+
+for i = 1:length(n)
+    h = (b-a)/n(i);
+    x = a:h:b;
+
+    trapetsregeln = @(B) h * ( 0.5*f(x(1), B) + sum(f(x(2:end-1), B)) + 0.5*f(x(end), B) );
+
+    rotationsvolym = @(B) pi * trapetsregeln(B) - riktigvolym;
+
+    % Ska lösa rotationsvolym(B) = 0
+
+    tolerans = 0.000001; % 5 deciamlers säkerhet
+    B_forst = B0;
+    B_gammal = B1;
+    B_ny = 0; 
+    rot_forst = rotationsvolym(B_forst);
+    
+    while abs(B_gammal - B_forst) > tolerans
+        rot_gammal = rotationsvolym(B_gammal);
+        
+        B_ny = B_gammal - rot_gammal * (B_gammal - B_forst) / (rot_gammal - rot_forst);
+        
+        B_forst = B_gammal;
+        rot_forst = rot_gammal;
+        B_gammal = B_ny;
+    end
+
+    B_funnen = B_gammal;
+    
+    olika_B(i) = B_funnen;
+end
+
+% Jämför de två sista värdena (för n=1024 och n=2048)
+B_bast = olika_B(end);
+B_nast_bast = olika_B(end-1);
+
+% Felskattningen är skillnaden mellan de två sista beräkningarna
+felskattning = abs(B_bast - B_nast_bast);
+
+fprintf('Slutgiltigt Beta-värde: %.10f\n', B_bast)
+fprintf('Bästa uppskattning av B (för n=%d): %.10f\n', n(end), B_bast);
+fprintf('Felskattning (abs|B_n - B_{n-1}|): %.2e\n', felskattning);
