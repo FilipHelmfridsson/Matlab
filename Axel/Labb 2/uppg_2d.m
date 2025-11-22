4cec2dba6183830b835e5a7e957b5a60267f51b9
abb22axerox <axelbsr2006@icloud.com>
2025-11-22 19:09:38 +0100

    push
    Co-authored-by: FilipHelmfridsson <FilipHelmfridsson@users.noreply.github.com>

--- Axel/Labb 2/uppg_2d.m
+++ Axel/Labb 2/uppg_2d.m
@@ -9,14 +9,16 @@ y_start = 2;
 x_start = 0;
 x_slut = pi/0.27;
 
-y = @(x) (exp(-B.*x) + 0.6) .* (1 + (x/9).^3);
+% y = @(x) (exp(-B.*x) + 0.6) .* (1 + (x/9).^3);
 % dy_dx = @(x) -B.*exp(-B.*x).*(1 + (x/9).^3) + (exp(-B.*x) + 0.6).*(x.^2 / 243); % derivatan
 
 % y' = ...
 diff_ekv = @(x, y) (3/2) * sin(y - 3 * x) + B * y;
 
-% Beräkna rotationsvolym med trapetsregeln
+% Löser differentialekvationen med inbyggd funktion, eftersom det inte fanns krav på annan
+[x_diff, y_diff] = ode45(diff_ekv, [x_start, x_slut], y_start);
 
+% Beräkna rotationsvolym med trapetsregeln
 n = [2,4,8,16,32,64,128,256,512,1024,2048];
 volymer = zeros(size(n));
 a = x_start;
@@ -24,12 +26,11 @@ b = x_slut;
 
 for i = 1:length(n)
     h = (b-a)/n(i);
-    x = a:h:b;
+    x_uniform = a:h:b;
 
-    % Löser differentialekvationen med inbyggd funktion, eftersom det inte fanns krav på annan
-    [x_diff, y_diff] = ode45(diff_ekv, x, y_start);
+    y_uniform = interp1(x_diff, y_diff, x_uniform); % Ger y-värden mellan två punkter x- och y-diff
 
-    volymintegrand = y_diff.^2;
+    volymintegrand = y_uniform.^2;
 
     trapetsregeln = h * ( 0.5*volymintegrand(1) + sum(volymintegrand(2:end-1)) + 0.5*volymintegrand(end) );
 
@@ -38,3 +39,12 @@ end
 
 disp(volymer)
 
+%% Felskattning
+delta_trunk = abs(diff(volymer(1:length(n))));
+
+kvoter = delta_trunk(1:end-1) ./ delta_trunk(2:end);
+
+disp('Abs skillnader:')
+disp(delta_trunk)
+disp('Kvoter:')
+disp(kvoter)
